072762680457ccef3c86f1bb19ab6ebe4f46fd9c
Ramdan <ramdan.firdaus@metrasat.co.id>
2019-04-27 09:39:25 +0700

    Sparse Array

---
+++ python/SparseArray.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the matchingStrings function below.
+def matchingStrings(strings, queries):
+    res = []
+    
+    for q in queries:
+        count = 0
+        for s in strings:
+            if q == s:
+                count += 1
+            
+        res.append(count)
+
+    return res
+    
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    strings_count = int(input())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    print('\n'.join(map(str, res)))
+    print('\n')
+
+    # fptr.close()
