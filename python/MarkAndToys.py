add96db603414a40ac6051a4cdd5e5ca68a1b360
Ramdan <ramdan.firdaus@metrasat.co.id>
2019-05-14 14:21:04 +0700

    Mark and toys

---
+++ python/MarkAndToys.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the maximumToys function below.
+def maximumToys(prices, k):
+    prices.sort()
+    count = 0
+    for price in prices:
+        if k >= price:
+            count += 1
+            k -= price
+    return count
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    prices = list(map(int, input().rstrip().split()))
+
+    result = maximumToys(prices, k)
+
+    print(str(result) + '\n')
+
+    # fptr.close()
