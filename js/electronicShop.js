65b365ff8b542dbff12b0942fb8f856185142e8c
Mohamad Ramdan Firdaus <mohamad.firdaus@grabtaxi.com>
2020-09-20 21:17:19 +0700

    Other solution from electronic shop

--- js/electronicShop.js
+++ js/electronicShop.js
@@ -22,6 +22,29 @@ function readLine() {
     return inputString[currentLine++];
 }
 
+/**
+ * Other solution
+ * function getMoneySpent(keyboards, drives, s){
+      // Complete this function
+      let max = -1;
+
+      // I'm trying to get the total number of keyboard and drive
+      // then compare to the money.
+      for(let i = 0; i < keyboards.length; i++){
+          var temp = 0;
+          for(let j = 0; j < drives.length; j++){
+              if(keyboards[i] + drives[j] <= s){
+                  temp = keyboards[i] + drives[j];
+                  max = temp > max ? temp : max;
+              }
+          }
+      }
+
+      return max;
+    }
+
+ */
+
 /*
  * Complete the getMoneySpent function below.
  */
