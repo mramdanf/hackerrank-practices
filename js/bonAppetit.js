710ef0cd33eea6b8719ff98cd84b16b4615b7b4e
Mohamad Ramdan Firdaus <mohamad.firdaus@grabtaxi.com>
2020-09-20 15:06:21 +0700

    Bon Appetit

---
+++ js/bonAppetit.js
@@ -0,0 +1,45 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the bonAppetit function below.
+function bonAppetit(bill, k, b) {
+    const billTmp = bill;
+    billTmp[k] = 0;
+    const sum = billTmp.reduce((a, b) => a + b, 0) / 2;
+    const diff = Math.abs(b - sum);
+    console.log(diff === 0 ? 'Bon Appetit' : diff)
+
+}
+
+function main() {
+    const nk = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(nk[0], 10);
+
+    const k = parseInt(nk[1], 10);
+
+    const bill = readLine().replace(/\s+$/g, '').split(' ').map(billTemp => parseInt(billTemp, 10));
+
+    const b = parseInt(readLine().trim(), 10);
+
+    bonAppetit(bill, k, b);
+}
