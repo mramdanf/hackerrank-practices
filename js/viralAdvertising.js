d2651364b75b47b7ea4e0a63244a466630c25b19
Mohamad Ramdan Firdaus <mohamad.firdaus@grabtaxi.com>
2020-09-22 17:31:28 +0700

    Viral advertising

---
+++ js/viralAdvertising.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the viralAdvertising function below.
+function viralAdvertising(n) {
+    let shared = 5;
+    let liked;
+    let cumulative = 0;
+    for (let i = 1; i <= n; i++) {
+        liked = Math.floor(shared / 2)
+        shared = liked * 3;
+        cumulative += liked;
+    }
+    return cumulative;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    let result = viralAdvertising(n);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
